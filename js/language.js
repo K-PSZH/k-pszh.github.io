6558e34c595ffadfa3865d89ccc8ad8de80d20ad
K-PSZH <master0emerald@gmail.com>
2026-01-28 22:41:30 +0300

    long story short (28jan26)

--- js/language.js
+++ js/language.js
@@ -1,28 +1,14 @@
 // Сама система i18n
-
 let currentLang = localStorage.getItem('lang') || 'ru';
 
 async function langLoad(lang) {
     const res = await fetch('/js/language.json');
     data = await res.json();
-
-    // Текстовые элементы
     document.querySelectorAll('[data-i18n]').forEach(e => {
         const key = e.dataset.i18n;
         e.innerHTML = data[lang][key];
     });
-
-    // Видео на главной странице (бесшовно)
-    const en = document.getElementById('title-front-en');
-    const ru = document.getElementById('title-front-ru');
-
-    const active = lang === 'ru' ? ru : en;
-    const inactive = lang === 'ru' ? en : ru;
-    active.currentTime = inactive.currentTime;
-
-    active.style.opacity = 1;
-    inactive.style.opacity = 0;
-
+    document.querySelectorAll('#lang-dropdown a').forEach(e => e.classList.toggle('active', e.dataset.lang === lang));
     document.documentElement.lang = lang;
 }
 
@@ -31,8 +17,6 @@ function langSet(lang) {
     currentLang = lang;
     localStorage.setItem('lang', lang);
     langLoad(lang);
-
-    // Подписи изображений в image viewer
     viewerCaption.textContent = data[lang][activeCaptionKey] || '';
 }
 
@@ -41,19 +25,21 @@ langLoad(currentLang);
 
 // Меню выбора языка
 function langMenu() {
-    document.getElementById("lang-dropdown").classList.toggle("show");
+    const dropdown = document.getElementById("lang-dropdown");
+    const btn = document.querySelector(".kpszh-header-dropdown-button");
+    dropdown.classList.toggle("display-block");
+    btn.classList.toggle("active", dropdown.classList.contains("display-block"));
 }
 
 // Пункты меню выбора языка
 window.onclick = e => {
-    if (!e.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("kpszh-header-dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
+    // если клик не по элементу внутри dropdown
+    if (!e.target.closest('.kpszh-header-dropdown')) {
+        const dropdown = document.getElementById("lang-dropdown");
+        const btn = document.querySelector(".kpszh-header-dropdown-button");
+        if (dropdown.classList.contains('display-block')) {
+            dropdown.classList.remove('display-block');
+            btn.classList.remove('active');
         }
     }
-}
+};
